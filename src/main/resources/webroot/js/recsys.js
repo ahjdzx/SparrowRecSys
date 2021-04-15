3bcdd39ec30932daad1d0a544e7834933f255ea8
Xin Zhang <ahjdzx1990@gmail.com>
2021-04-15 10:43:48 +0800

    Add popularity sort type

--- src/main/resources/webroot/js/recsys.js
+++ src/main/resources/webroot/js/recsys.js
@@ -99,7 +99,7 @@ function addRowFrameWithoutLink(pageId, rowName, rowId, baseUrl) {
 
 function addGenreRow(pageId, rowName, rowId, size, baseUrl) {
     addRowFrame(pageId, rowName, rowId, baseUrl);
-    $.getJSON(baseUrl + "getrecommendation?genre="+rowName+"&size="+size+"&sortby=rating", function(result){
+    $.getJSON(baseUrl + "getrecommendation?genre="+rowName+"&size="+size+"&sortby=popularity", function(result){
         $.each(result, function(i, movie){
           appendMovie2Row(rowId, movie.title, movie.movieId, movie.releaseYear, movie.averageRating.toPrecision(2), movie.ratingNumber, movie.genres,baseUrl);
         });
